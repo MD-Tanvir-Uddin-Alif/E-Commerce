bcfb50b6fb1c933f1b15f15fba7129d1871805de
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-03-01 14:54:15 +0600

    Product app and database created

--- ecommarce/settings.py
+++ ecommarce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'user',
+    'Product',
 ]
 
 # REST Framework settings
@@ -131,7 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
