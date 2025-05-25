342f5d1f00ae9dc066f5f889224dd614f611bbb5
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-05-25 10:01:37 +0600

    Admin problem solved

--- User/admin.py
+++ User/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import UserModel
 # Register your models here.
+
+@admin.register(UserModel)
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_username', 'get_email', 'phone_number')
+    search_fields = ('username',)
+    
+    def get_username(self, obj):
+        return obj.user.username
+    get_username.short_description = 'Username'
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
--- User/models.py
+++ User/models.py
@@ -7,4 +7,4 @@ class UserModel(models.Model):
     phone_number = models.CharField(max_length=12)
     
     def __str__(self):
-        return self.user
+        return self.user.username
