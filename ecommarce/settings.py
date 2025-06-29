5c932a65858b99d125d0bb4567acccf7bbdd2411
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-06-29 18:01:37 +0600

    fixed a fioolish mistake

--- ecommarce/settings.py
+++ ecommarce/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,6 +153,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='alif252669@gmail.com'
-EMAIL_HOST_PASSWORD = 'zlahnrywhpkvxxxl'
-DEFAULT_FROM_EMAIL ='alif252669@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
